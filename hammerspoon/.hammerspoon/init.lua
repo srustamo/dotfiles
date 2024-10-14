797ad68d7c9cf43ca9ec8f16ed8eb7af6ee4d203
rushhike <rustamoff@gmail.com>
2024-10-14 14:16:47 +0500

    Tridactyl blacklist, hammerspoon Zed

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -5,10 +5,10 @@ require "pomodoor"
 require "atreus"
 
 -- init grid
-hs.grid.MARGINX 	= 0
-hs.grid.MARGINY 	= 0
-hs.grid.GRIDWIDTH 	= 9
-hs.grid.GRIDHEIGHT 	= 4
+hs.grid.MARGINX             = 0
+hs.grid.MARGINY             = 0
+hs.grid.GRIDWIDTH           = 9
+hs.grid.GRIDHEIGHT          = 4
 
 -- disable animation
 hs.window.animationDuration = 0
@@ -17,23 +17,23 @@ hs.window.animationDuration = 0
 -- hotkey mash
 -- the original was {"ctrl", "alt"} - conflicts with HashTWM
 -- local mash       = {"ctrl", "alt"}
-local mash       = {"cmd", "ctrl"}
-local mash_app 	 = {"cmd", "alt", "ctrl"}
-local mash_shift = {"ctrl", "alt", "shift"}
-local mash_test	 = {"cntrl", "shift"}	
-local hyper	 = {"cmd", "alt", "shift", "ctrl"}	
+local mash       = { "cmd", "ctrl" }
+local mash_app   = { "cmd", "alt", "ctrl" }
+local mash_shift = { "ctrl", "alt", "shift" }
+local mash_test  = { "cntrl", "shift" }
+local hyper      = { "cmd", "alt", "shift", "ctrl" }
 
 hs.loadSpoon("SpoonInstall")
-Install=spoon.SpoonInstall
+Install = spoon.SpoonInstall
 Install:andUse("DismissNotifications", {
     hotkeys = {
-        all = {hyper, "'"}
+        all = { hyper, "'" }
     }
 })
 
 Install:andUse("MenuBar", {
     hotkeys = {
-        menubar_toggle = {hyper, "m"}
+        menubar_toggle = { hyper, "m" }
     }
 })
 Install:andUse("FadeLogo", {
@@ -50,69 +50,76 @@ Install:andUse("FadeLogo", {
 -- })
 --------------------------------------------------------------------------------
 appCuts = {
-  -- d = 'Dictionary',
-  t = 'iterm',
-  r = 'Google chrome',
-  f = 'Firefox',
-  -- t = 'Trello X',
-  -- 4 reserved for dash shortcut 
-  -- q = 'Quiver',
-  e = 'emacs',
-  -- e = 'Rush_Emacs',
-  -- e = '/Applications/Emacs.app/Contents/MacOS/Emacs --no-init-file -l "~/Dropbox (Personal)/source/rush-emacs-srmbp2/rush-emacs/init.el" >/dev/null 2>&1 &',
-  a = 'Finder',
-  v = 'VSCodium',
-  b = 'Obsidian',
-  -- i = 'Terminal'
+    -- d = 'Dictionary',
+    -- t = 'iterm',
+    t = 'wezterm',
+    r = 'Google chrome',
+    f = 'Firefox',
+    -- t = 'Trello X',
+    -- 4 reserved for dash shortcut
+    -- q = 'Quiver',
+    e = 'emacs',
+    -- e = 'Rush_Emacs',
+    -- e = '/Applications/Emacs.app/Contents/MacOS/Emacs --no-init-file -l "~/Dropbox (Personal)/source/rush-emacs-srmbp2/rush-emacs/init.el" >/dev/null 2>&1 &',
+    a = 'Finder',
+    v = 'VSCodium',
+    b = 'Obsidian',
+    -- i = 'Terminal'
 }
 
 function yabai(args)
-
-  hs.task.new("/usr/local/bin/yabai",nil, function(ud, ...)
-    print("stream", hs.inspect(table.pack(...)))
-    return true
-  end, args):start()
-
+    hs.task.new("/usr/local/bin/yabai", nil, function(ud, ...)
+        print("stream", hs.inspect(table.pack(...)))
+        return true
+    end, args):start()
 end
+
 -- https://github.com/camspiers/dotfiles/blob/master/files/.config/skhd/skhdrc
 -- hs.hotkey.bind(hyper, "j", function() yabai({"-m", "window", "--focus", "north"}) end)
 -- hs.hotkey.bind(hyper, "k", function() yabai({"-m", "window", "--focus", "south"}) end)
 -- hs.hotkey.bind(hyper, "h", function() yabai({"-m", "window", "--focus", "west"}) end)
 -- hs.hotkey.bind(hyper, "l", function() yabai({"-m", "window", "--focus", "east"}) end)
 
-hs.hotkey.bind(mash, "k", function() yabai({"-m", "window", "--focus", "north"}) end)
-hs.hotkey.bind(mash, "j", function() yabai({"-m", "window", "--focus", "south"}) end)
-hs.hotkey.bind(mash, "h", function() yabai({"-m", "window", "--focus", "west"}) end)
-hs.hotkey.bind(mash, "l", function() yabai({"-m", "window", "--focus", "east"}) end)
+hs.hotkey.bind(mash, "k", function() yabai({ "-m", "window", "--focus", "north" }) end)
+hs.hotkey.bind(mash, "j", function() yabai({ "-m", "window", "--focus", "south" }) end)
+hs.hotkey.bind(mash, "h", function() yabai({ "-m", "window", "--focus", "west" }) end)
+hs.hotkey.bind(mash, "l", function() yabai({ "-m", "window", "--focus", "east" }) end)
 -- hs.hotkey.bind(mash, "r", function() yabai({"-m", "window", "--focus", "recent"}) end)
 -- hs.hotkey.bind(mash, "g", function() yabai({"-m", "window", "--grid", "1:3:0:0:2:1"}) end)
-hs.hotkey.bind(mash, "f", function() yabai({"-m", "window", "--toggle", "zoom-fullscreen"}) end)
-hs.hotkey.bind(mash, "t", function() yabai({"-m", "window", "--toggle", "float"},{"-m", "window", "--grid", "4:4:1:1:2:2"}) end)
+hs.hotkey.bind(mash, "f", function() yabai({ "-m", "window", "--toggle", "zoom-fullscreen" }) end)
+hs.hotkey.bind(mash, "t",
+    function() yabai({ "-m", "window", "--toggle", "float" }, { "-m", "window", "--grid", "4:4:1:1:2:2" }) end)
 -- space requires scripting addition
--- hs.hotkey.bind(mash, "1", function() yabai({"-m", "space", "--focus", "1"}) end) 
+-- hs.hotkey.bind(mash, "1", function() yabai({"-m", "space", "--focus", "1"}) end)
 -- hs.hotkey.bind(mash, "2", function() yabai({"-m", "space", "--focus", "2"}) end)
-hs.hotkey.bind(mash, "w", function() yabai({"-m", "window", "--swap", "west"}) end)
-hs.hotkey.bind(mash, "e", function() yabai({"-m", "window", "--swap", "east"}) end)
-hs.hotkey.bind(mash, "n", function() yabai({"-m", "window", "--swap", "north"}) end)
-hs.hotkey.bind(mash, "s", function() yabai({"-m", "window", "--swap", "south"}) end)
+hs.hotkey.bind(mash, "w", function() yabai({ "-m", "window", "--swap", "west" }) end)
+hs.hotkey.bind(mash, "e", function() yabai({ "-m", "window", "--swap", "east" }) end)
+hs.hotkey.bind(mash, "n", function() yabai({ "-m", "window", "--swap", "north" }) end)
+hs.hotkey.bind(mash, "s", function() yabai({ "-m", "window", "--swap", "south" }) end)
 -- hs.hotkey.bind(mash, "e", function() yabai({"-m", "window", "--swap", "east"}) end)
-hs.hotkey.bind(mash, "a", function() yabai({"-m", "window", "--resize", "left:-40:0"}) end)
+hs.hotkey.bind(mash, "a", function() yabai({ "-m", "window", "--resize", "left:-40:0" }) end)
 -- hs.hotkey.bind(mash, "s", function() yabai({"-m", "window", "--resize", "bottom:0:40"}) end)
 -- hs.hotkey.bind(mash, "s", function() yabai({"-m", "window", "--resize", "bottom:0:40"}) end)
-hs.hotkey.bind(mash, "space", function() yabai({"-m", "space", "--balance"}) end)
-hs.hotkey.bind(mash, "r", function() yabai({"-m", "space", "--rotate", "90"}) end)
+hs.hotkey.bind(mash, "space", function() yabai({ "-m", "space", "--balance" }) end)
+hs.hotkey.bind(mash, "r", function() yabai({ "-m", "space", "--rotate", "90" }) end)
 
 -- hs.hotkey.bind(mash,  "1", function()  yabai({ "-m", "window", "--space", "--focus", "1"}) end)
-hs.hotkey.bind(mash,  "1", function()  yabai({ "-m", "window", "--space",  "1"}) end, function() yabai({ "-m", "space", "--focus", "1"}) end)
-hs.hotkey.bind(mash,  "2", function()  yabai({ "-m", "window", "--space",  "2"}) end, function() yabai({ "-m", "space", "--focus", "2"}) end)
+hs.hotkey.bind(mash, "1", function() yabai({ "-m", "window", "--space", "1" }) end,
+    function() yabai({ "-m", "space", "--focus", "1" }) end)
+hs.hotkey.bind(mash, "2", function() yabai({ "-m", "window", "--space", "2" }) end,
+    function() yabai({ "-m", "space", "--focus", "2" }) end)
 -- hs.hotkey.bind(mash,  "2", function()  yabai({ "-m", "window", "--space", "2"; "yabai", "-m", "space", "--focus", "2" }) end)
-hs.hotkey.bind(mash,  "3", function()  yabai({ "-m", "window", "--space",  "3"}, { "-m", "window", "--space", " --focus", "3"}) end)
-hs.hotkey.bind(mash,  "4", function()  yabai({ "-m", "window", "--space",  "4"}, { "-m", "window", "--space", " --focus", "4"}) end)
-hs.hotkey.bind(mash,  "5", function()  yabai({ "-m", "window", "--space",  "5"}, { "-m", "window", "--space", " --focus", "5"}) end)
-hs.hotkey.bind(mash,  "6", function()  yabai({ "-m", "window", "--space",  "6"}, { "-m", "window", "--space", " --focus", "6"}) end)
+hs.hotkey.bind(mash, "3",
+    function() yabai({ "-m", "window", "--space", "3" }, { "-m", "window", "--space", " --focus", "3" }) end)
+hs.hotkey.bind(mash, "4",
+    function() yabai({ "-m", "window", "--space", "4" }, { "-m", "window", "--space", " --focus", "4" }) end)
+hs.hotkey.bind(mash, "5",
+    function() yabai({ "-m", "window", "--space", "5" }, { "-m", "window", "--space", " --focus", "5" }) end)
+hs.hotkey.bind(mash, "6",
+    function() yabai({ "-m", "window", "--space", "6" }, { "-m", "window", "--space", " --focus", "6" }) end)
 -- Launch applications
 for key, app in pairs(appCuts) do
-  hs.hotkey.bind(mash_app, key, function () hs.application.launchOrFocus(app) end)
+    hs.hotkey.bind(mash_app, key, function() hs.application.launchOrFocus(app) end)
 end
 
 -- global operations
@@ -306,24 +313,53 @@ end
 ----------------------
 -- from docs example
 -- k = hs.hotkey.modal.new({"cmd", "shift", "ctrl", "alt"}, 'I')
-i = hs.hotkey.modal.new({"cmd"}, 'I')
+i = hs.hotkey.modal.new({ "cmd" }, 'I')
 -- function i:entered() hs.alert('I mode', 10)
-function i:entered() hs.alert('I mode', 'A')
+function i:entered()
+    hs.alert('I mode', 'A')
 end
+
 function i:exited() --hs.alert'I exited '
-   hs.alert.closeAll() 
+    hs.alert.closeAll()
 end
+
 i:bind('', 'escape', function() i:exit() end)
-i:bind('', 'J', 'Pressed J',function() print'let the record show that J was pressed' end)
-i:bind('', 'i', function() hs.application.launchOrFocus('iterm') i:exit() end)
-i:bind('', 'E', function() hs.application.launchOrFocus('Emacs') i:exit() end)
+i:bind('', 'J', 'Pressed J', function() print 'let the record show that J was pressed' end)
+-- i:bind('', 'i', function() hs.application.launchOrFocus('iterm') i:exit() end)
+i:bind('', 'i', function()
+    hs.application.launchOrFocus('wezterm')
+    i:exit()
+end)
+i:bind('', 'E', function()
+    hs.application.launchOrFocus('Emacs')
+    i:exit()
+end)
 -- i:bind('', 'R', function() hs.application.launchOrFocus('Google chrome') i:exit() end)
-i:bind('', 'R', function() hs.application.launchOrFocus('Brave Browser') i:exit() end)
-i:bind('', 'F', function() hs.application.launchOrFocus('Firefox') i:exit() end)
-i:bind('', 'A', function() hs.application.launchOrFocus('Finder') i:exit() end)
-i:bind('', 'V', function() hs.application.launchOrFocus('VSCodium') i:exit() end)
-i:bind('', 'b', function() hs.application.launchOrFocus('Obsidian') i:exit() end)
-i:bind('', 't', function() hs.application.launchOrFocus('Telegram Lite') i:exit() end)
+-- i:bind('', 'R', function() hs.application.launchOrFocus('Brave Browser') i:exit() end)
+i:bind('', 'Z', function()
+    hs.application.launchOrFocus('Zed')
+    i:exit()
+end)
+i:bind('', 'F', function()
+    hs.application.launchOrFocus('Firefox')
+    i:exit()
+end)
+i:bind('', 'A', function()
+    hs.application.launchOrFocus('Finder')
+    i:exit()
+end)
+i:bind('', 'V', function()
+    hs.application.launchOrFocus('VSCodium')
+    i:exit()
+end)
+i:bind('', 'b', function()
+    hs.application.launchOrFocus('Obsidian')
+    i:exit()
+end)
+i:bind('', 't', function()
+    hs.application.launchOrFocus('Telegram Lite')
+    i:exit()
+end)
 
 -- -- k:bind('', 'I', 'Select app',function()
 -- --   -- Launch applications
@@ -364,13 +400,13 @@ i:bind('', 't', function() hs.application.launchOrFocus('Telegram Lite') i:exit(
 
 ---------------------------------
 
-hs.alert.defaultStyle.strokeColor =  {white = 1, alpha = 0.75}
-hs.alert.defaultStyle.fillColor =  {white = 0.05, alpha = 0.75}
+hs.alert.defaultStyle.strokeColor = { white = 1, alpha = 0.75 }
+hs.alert.defaultStyle.fillColor = { white = 0.05, alpha = 0.75 }
 hs.alert.defaultStyle.radius = 10
 hs.alert.defaultStyle.strokeWidth = 1
 
 
-hs.alert.defaultStyle.textColor = {white = 25, alpha = 0.25}
+hs.alert.defaultStyle.textColor = { white = 25, alpha = 0.25 }
 
 -- 11-06-2018 https://gist.github.com/tmiller/31409a34e28ad7381d9ce0bde48d8254
 
@@ -379,7 +415,7 @@ local screenMode = hs.hotkey.modal.new('cmd', 'j')
 
 function screenMode:entered()
     alertUuids = hs.fnutils.imap(hs.screen.allScreens(), function(screen)
-       return hs.alert.show('Move Window', hs.alert.defaultStyle, screen, true)
+        return hs.alert.show('Move Window', hs.alert.defaultStyle, screen, true)
     end)
 end
 
@@ -390,21 +426,24 @@ function screenMode:exited()
 end
 
 grid = {
-    { key='j', unit=hs.geometry.rect(0, 0.5, 1, 0.5) },
-    { key='k', unit=hs.geometry.rect(0, 0, 1, 0.5) },
-    { key='h', unit=hs.layout.left50 },
-    { key='l', unit=hs.layout.right50 },
+    { key = 'j',   unit = hs.geometry.rect(0, 0.5, 1, 0.5) },
+    { key = 'k',   unit = hs.geometry.rect(0, 0, 1, 0.5) },
+    { key = 'h',   unit = hs.layout.left50 },
+    { key = 'l',   unit = hs.layout.right50 },
+    -- { key='r', unit=hs.layout.left30 },
+    -- { key='e', unit=hs.layout.right30 },
+    { key = 'r',   unit = hs.geometry.rect(0.3, 0, 0.33, 1) },
 
-    { key='y', unit=hs.geometry.rect(0, 0, 0.5, 0.5) },
-    { key='u', unit=hs.geometry.rect(0.5, 0, 0.5, 0.5) },
-    { key='n', unit=hs.geometry.rect(0, 0.5, 0.5, 0.5) },
-    { key='m', unit=hs.geometry.rect(0.5, 0.5, 0.5, 0.5) },
+    { key = 'y',   unit = hs.geometry.rect(0, 0, 0.5, 0.5) },
+    { key = 'u',   unit = hs.geometry.rect(0.5, 0, 0.5, 0.5) },
+    { key = 'n',   unit = hs.geometry.rect(0, 0.5, 0.5, 0.5) },
+    { key = 'm',   unit = hs.geometry.rect(0.5, 0.5, 0.5, 0.5) },
 
-    { key='space', unit=hs.layout.maximized }
+    { key = 'space', unit = hs.layout.maximized }
 }
 
 hs.fnutils.each(grid, function(entry)
-   screenMode:bind('', entry.key, function()
+    screenMode:bind('', entry.key, function()
         hs.window.focusedWindow():moveToUnit(entry.unit)
         -- screenMode:exit()
     end)
@@ -427,37 +466,38 @@ screenMode:bind('cmd', 'j', function()
     screenMode:exit()
 end)
 
-screenMode:bind('', '.', function() screenMode:exit()
-                   hs.hints.windowHints()
-                   -- screenMode:exit()
+screenMode:bind('', '.', function()
+    screenMode:exit()
+    hs.hints.windowHints()
+    -- screenMode:exit()
 end)
 
-screenMode:bind('', 'i', function() 
-                   hs.grid.pushWindowNextScreen()
-                   -- screenMode:exit()
+screenMode:bind('', 'i', function()
+    hs.grid.pushWindowNextScreen()
+    -- screenMode:exit()
 end)
 
-screenMode:bind('', 'o', function() 
-                   hs.grid.pushWindowPrevScreen()
-                   -- screenMode:exit()
+screenMode:bind('', 'o', function()
+    hs.grid.pushWindowPrevScreen()
+    -- screenMode:exit()
 end)
-    -- { key='i', unit=hs.grid.pushWindowNextSceen()},
-    -- { key='o', unit=hs.grid.pushWindowPrevScreen()},
-    -- { key='.', unit=hs.hints.windowHints},
-    --
+-- { key='i', unit=hs.grid.pushWindowNextSceen()},
+-- { key='o', unit=hs.grid.pushWindowPrevScreen()},
+-- { key='.', unit=hs.hints.windowHints},
+--
 screenMode:bind('', 'g', function()
-                   hs.grid.show()
-                   -- screenMode:exit()
+    hs.grid.show()
+    -- screenMode:exit()
 end)
 
 screenMode:bind('', 't', function()
-                   hs.grid.adjustWidth( 1)
-                   -- screenMode:exit()
+    hs.grid.adjustWidth(1)
+    -- screenMode:exit()
 end)
 
 screenMode:bind('', 'b', function()
-                   hs.grid.adjustWidth(-1)
-                   -- screenMode:exit()
+    hs.grid.adjustWidth(-1)
+    -- screenMode:exit()
 end)
 ----------------------------
 -- local spaces = require("hs._asm.undocumented.spaces")
@@ -474,14 +514,15 @@ end)
 spaces = require("hs.spaces")
 -- move current window to the space sp
 function MoveWindowToSpace(sp)
-  local win = hs.window.focusedWindow()      -- current window
-  local cur_screen = hs.screen.mainScreen()
-  local cur_screen_id = cur_screen:getUUID()
-  local all_spaces=spaces.allSpaces()
-  local spaceID = all_spaces[cur_screen_id][sp]
-  spaces.moveWindowToSpace(win:id(), spaceID)
-  spaces.gotoSpace(spaceID)              -- follow window to new space
+    local win = hs.window.focusedWindow() -- current window
+    local cur_screen = hs.screen.mainScreen()
+    local cur_screen_id = cur_screen:getUUID()
+    local all_spaces = spaces.allSpaces()
+    local spaceID = all_spaces[cur_screen_id][sp]
+    spaces.moveWindowToSpace(win:id(), spaceID)
+    spaces.gotoSpace(spaceID) -- follow window to new space
 end
+
 hs.hotkey.bind(mash, '1', function() MoveWindowToSpace(1) end)
 hs.hotkey.bind(mash, '2', function() MoveWindowToSpace(2) end)
 hs.hotkey.bind(mash, '3', function() MoveWindowToSpace(3) end)
